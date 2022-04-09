3e83ce54dca5b4fcb94160ef5f68f19b5766a1be
Felix Bade <flix.bade@gmail.com>
2022-04-09 19:30:56 +0300

    Check flag

--- app/controllers/challenge.py
+++ app/controllers/challenge.py
@@ -1,5 +1,5 @@
 import os
-from flask import render_template
+from flask import render_template, request
 
 from app import app
 
@@ -9,3 +9,16 @@ def view_brief(name):
     brief = open(os.path.join(challenge_path, 'brief.md')).read()
     uri = open(os.path.join(challenge_path, 'uri.txt')).read()
     return render_template('challenge-brief.html', brief=brief, uri=uri)
+
+@app.route('/challenges/<name>', methods=['POST'])
+def check_brief(name):
+    attempted_flag = request.form.get('flag')
+
+    challenge_path = os.path.join('puzzle', 'challenges', name)
+    correct_flag = open(os.path.join(challenge_path, 'flag.txt')).read()
+
+    if attempted_flag == correct_flag:
+        return 'Correct!'
+    else:
+        return 'Incorrect!'
+    
