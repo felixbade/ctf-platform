a440dfdfcb8c035f6daaece05e2f1d2d625ff807
Joel Juhela <joel.juhela@protonmail.com>
2022-04-09 21:21:09 +0300

    Enable CSRF protection

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_wtf import CSRFProtect
 
 from config import Config
 
@@ -11,5 +12,6 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager(app)
+csrf = CSRFProtect(app)
 
 from app import controllers
