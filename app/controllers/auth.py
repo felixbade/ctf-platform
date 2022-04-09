9ba24e9f259f2254f1592a80e17da4395818ac7f
Joel Juhela <joel.juhela@protonmail.com>
2022-04-09 21:37:11 +0300

    Cleanup controllers/auth.py

--- app/controllers/auth.py
+++ app/controllers/auth.py
@@ -1,8 +1,9 @@
 from flask import render_template, redirect, request
 
 from app.models.user import User
-from app.forms import RegistrationForm
+from app.forms import RegistrationForm, LoginForm
 from app import app, db, login_manager
+
 
 @login_manager.user_loader
 def get_user(user_id):
@@ -12,9 +13,7 @@ def get_user(user_id):
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     form = RegistrationForm()
-    print("hello")
     if form.validate_on_submit():
-        print("valid form")
         user = User(username=form.username.data)
         user.set_password(form.password1.data)
         db.session.add(user)
