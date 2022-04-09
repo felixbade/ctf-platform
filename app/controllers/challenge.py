260c2083ff6ba20a9801352bec41c8737a14fda5
Felix Bade <flix.bade@gmail.com>
2022-04-09 19:26:46 +0300

    Add challenge brief page

--- app/controllers/challenge.py
+++ app/controllers/challenge.py
@@ -5,6 +5,7 @@ from app import app
 
 @app.route('/challenges/<name>')
 def view_brief(name):
-    filename = os.path.join('puzzle', 'challenges', name, 'brief.md')
-    welcome = open(filename).read()
-    return render_template('markdown-test.html', article=welcome)
+    challenge_path = os.path.join('puzzle', 'challenges', name)
+    brief = open(os.path.join(challenge_path, 'brief.md')).read()
+    uri = open(os.path.join(challenge_path, 'uri.txt')).read()
+    return render_template('challenge-brief.html', brief=brief, uri=uri)
