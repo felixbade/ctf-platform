b550762f38567696d5f95e467dbaf002e45ad58f
Joel Juhela <joel.juhela@protonmail.com>
2022-04-12 23:06:22 +0300

    Allow users to change their passwords

--- app/controllers/auth.py
+++ app/controllers/auth.py
@@ -1,9 +1,9 @@
 from flask import render_template, redirect, flash
-from flask_login import login_user, login_required, logout_user
+from flask_login import login_user, login_required, logout_user, current_user
 from sqlalchemy.exc import IntegrityError
 
 from app.models.user import User
-from app.forms import RegistrationForm, LoginForm
+from app.forms import RegistrationForm, LoginForm, ChangePasswordForm
 from app import app, db, login_manager
 
 
@@ -42,6 +42,22 @@ def login():
             return render_template('auth/login.html', form=form)
 
     return render_template('auth/login.html', form=form)
+
+
+@app.route('/change-password', methods=['GET', 'POST'])
+@login_required
+def change_password():
+    form = ChangePasswordForm()
+    wrong_password = False
+    if form.validate_on_submit():
+        if current_user.check_password(form.old_password.data):
+            current_user.set_password(form.new_password1.data)
+            db.session.commit()
+            return redirect('/')
+        else:
+            wrong_password = True
+
+    return render_template('auth/change-password.html', form=form, wrong_password=wrong_password)
 
 
 @app.route('/logout')
