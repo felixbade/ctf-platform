49cd99e9bf8c7a1e339dd0bde5146fb9f773e764
Joel Juhela <joel.juhela@protonmail.com>
2022-04-16 14:18:31 +0300

    Dirty hack to fix #91 for this run

--- app/templatetags.py
+++ app/templatetags.py
@@ -1,5 +1,5 @@
-from humanize.time import precisedelta, naturaltime, naturaldate
-from datetime import datetime, timedelta
+from humanize.time import naturaltime, naturaldate
+from datetime import datetime, timedelta, timezone
 
 from app import app
 
@@ -15,6 +15,8 @@ def humanize_time(value):
         if delta < timedelta(hours=1):
             return naturaltime(delta, minimum_unit='seconds')
         else:
-            return f"{naturaldate(value)} at {value.strftime('%H:%M')}"
+            # This is a dirty hack, that works because we know the server is in UTC
+            localized = value.replace(tzinfo=timezone.utc).astimezone(tz=None)
+            return f"{naturaldate(localized)} at {localized.strftime('%H:%M')}"
     else:
         return ""
